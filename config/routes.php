554e02d72f048b47c1e1f95f4896f209b214a323
ad-pgn <adrian.pogneau@gmail.com>
2026-08-29 15:52:34 +0200

    feat: liste des agences pour l'administrateur

--- config/routes.php
+++ config/routes.php
@@ -10,11 +10,10 @@ $router->get('/deconnexion', 'AuthController@deconnexion');
 
 $router->get('/trajets/creer', 'TrajetController@formulaireCreation');
 $router->post('/trajets/creer', 'TrajetController@creer');
-
 $router->get('/trajets/:id/modifier', 'TrajetController@formulaireModification');
 $router->post('/trajets/:id/modifier', 'TrajetController@modifier');
-
 $router->post('/trajets/:id/supprimer', 'TrajetController@supprimer');
 
 $router->get('/admin', 'AdminController@index');
 $router->get('/admin/utilisateurs', 'AdminController@utilisateurs');
+$router->get('/admin/agences', 'AdminController@agences');
